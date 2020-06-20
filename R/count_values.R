610cbc77e209d0204b2b28b0c890112e3773b8a2
averyrobbins1 <averyrobbins17@gmail.com>
2020-06-20 16:36:38 -0600

    finished

---
+++ R/count_values.R
@@ -0,0 +1,22 @@
+#' Count values in multiple columns
+#'
+#' @param data A data.frame or tibble
+#' @param ... Columns that you want to count the values of
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' count_values(mtcars, carb, vs, am)
+count_values <- function(data, ...) {
+
+  `%>%` <- magrittr::`%>%`
+
+  dplyr::select(data, ...) %>%
+    dplyr::mutate(dplyr::across(dplyr::everything(), as.character)) %>%
+    purrr::map_dfr(
+      ~ dplyr::count(
+        tibble::tibble(value = .x), value, sort = TRUE
+        ), .id = "variable"
+      )
+}
--- packages.R
+++
@@ -1,3 +0,0 @@
-lemon
-marissa
-ggrepel
